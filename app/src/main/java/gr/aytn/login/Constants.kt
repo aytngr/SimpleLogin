325c39c9266f0ddccff5d3ecd60bb7f38cb5b6ba
aytngr <aytengaraisayeva@gmail.com>
2022-05-26 14:30:01 +0400

    Initial commit

---
+++ app/src/main/java/gr/aytn/login/Constants.kt
@@ -0,0 +1,6 @@
+package gr.aytn.login
+
+object Constants {
+    const val EMAIL: String = "emanat@emanat.az"
+    const val PASSWORD : String = "password123"
+}
---
+++ app/src/main/java/gr/aytn/login/MainActivity.kt
@@ -0,0 +1,49 @@
+package gr.aytn.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val correctEmail = Constants.EMAIL
+        val correctPassword = Constants.PASSWORD
+
+        val etEmail: EditText = findViewById(R.id.email)
+        val etPassword: EditText = findViewById(R.id.password)
+        val tvMessage: TextView = findViewById(R.id.message)
+        val btnLogin : Button = findViewById(R.id.login)
+
+
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if(email == correctEmail && password == correctPassword){
+                /*
+                tvMessage.text = "Siz uğurla daxil oldunuz!"
+                tvMessage.background = ContextCompat.getDrawable(this,R.drawable.correct_credentials_message
+                 */
+                startActivity(Intent(this,LoginSuccess::class.java))
+                finish()
+            }else{
+                if(email != correctEmail) {
+                    etEmail.error = "Email yanlışdır!"
+                }
+                if(password != correctPassword){
+                    etPassword.error = "Şifrə yanlışdır!"
+                }
+                tvMessage.text = "Email və ya şifrə yanlışdır!"
+                tvMessage.background = ContextCompat.getDrawable(this,R.drawable.wrong_credentials_message)
+            }
+        }
+    }
+}
